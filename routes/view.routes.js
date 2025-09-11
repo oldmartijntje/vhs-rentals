d84ca8b585bec1456eef3882859b48111bc9fe44
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-11 15:23:48 +0200

    new login template

--- routes/view.routes.js
+++ routes/view.routes.js
@@ -5,7 +5,10 @@ viewRouter.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 viewRouter.get('/login', (req, res) => {
-    res.render('login', { title: 'Login' });
+    res.render('loginpage/customer-login', { title: 'Login' });
+});
+viewRouter.get('/staff/login', (req, res) => {
+    res.render('loginpage/staff-login', { title: 'Login' });
 });
 viewRouter.get('/About', (req, res) => {
     res.render('about', { title: 'About' });
