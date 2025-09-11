b37d6f0a593cc6da2a4b0c4434f0a25d7bbfba7d
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-11 10:05:25 +0200

    remove examples

--- routes/a.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as aController from '../controllers/a.controller.js';
-const router = express.Router();
-
-router.get('/a', aController.getA);
-router.get('/b', aController.getB);
-
-export default router;
