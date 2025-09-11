2b1b7960d14bed13fbdbd5957d008322c5b44955
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-11 15:58:50 +0200

    forgot to fix pathing

--- routes/view.routes.js
+++ routes/view.routes.js
@@ -2,14 +2,14 @@ import { Router } from 'express';
 export const viewRouter = Router();
 
 viewRouter.get('/', (req, res) => {
-    res.render('home', { title: 'Home' });
+    res.render('pages/home', { title: 'Home' });
 });
 viewRouter.get('/login', (req, res) => {
-    res.render('loginpage/customer-login', { title: 'Login' });
+    res.render('pages/loginpage/customer-login', { title: 'Login' });
 });
 viewRouter.get('/staff/login', (req, res) => {
-    res.render('loginpage/staff-login', { title: 'Login' });
+    res.render('pages/loginpage/staff-login', { title: 'Login' });
 });
 viewRouter.get('/About', (req, res) => {
-    res.render('about', { title: 'About' });
+    res.render('pages/about', { title: 'About' });
 });
