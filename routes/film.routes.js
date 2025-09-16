c12b7972b761244bfb286fd842eb7a0702b710e0
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-16 12:10:46 +0200

    film routes

--- routes/film.routes.js
+++ routes/film.routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { newestFilmsRequest, filmInfoRequest } from '../controllers/film.controller.js';
+import { newestFilmsRequest, filmInfoRequest, postFilm, putFilm, deleteFilm } from '../controllers/film.controller.js';
 
 const filmRouter = express.Router();
 
 
 filmRouter.get("/newest", newestFilmsRequest);
 filmRouter.get("", filmInfoRequest);
+filmRouter.post("", postFilm);
+filmRouter.delete("", deleteFilm);
+filmRouter.put("", putFilm);
 
 
 
