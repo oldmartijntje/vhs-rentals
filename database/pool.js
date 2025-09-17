50338d3c761614a4a3c49f232659cd24b219e9d8
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-17 20:39:36 +0200

    checking whether .env exists

--- database/pool.js
+++ database/pool.js
@@ -1,8 +1,21 @@
 
 import dotenv from 'dotenv';
 import mysql from 'mysql2';
+import { logger } from '../middleware/logger.js';
 dotenv.config();
-
+logger.debug(`Database config: ${JSON.stringify({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    multipleStatements: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0,
+})}`)
 // Create a connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
