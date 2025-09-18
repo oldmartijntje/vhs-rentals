5917a02cbe2fbc04a79b49988e8f541c251832bc
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-18 11:43:32 +0200

    only 1 movie, so that it is not permanetn to the db

--- cypress/e2e/staff-edit-film.cy.js
+++ cypress/e2e/staff-edit-film.cy.js
@@ -42,6 +42,8 @@ describe('Staff Edit Film CRUD Access', () => {
             }
         });
 
+        let filmId;
+
         it('can add a new film', () => {
             cy.visit('http://localhost:6969/Staff/Edit/Film');
             cy.get('#title').type(filmData.title);
@@ -54,53 +56,26 @@ describe('Staff Edit Film CRUD Access', () => {
             cy.get('#actors').type(filmData.actors);
             cy.get('button[type="submit"]').contains('Add Film').click();
             cy.url({ timeout: 10000 }).should('include', '/Staff/Edit/Film?v=');
-        });
-
-        it('can edit an existing film', () => {
-            // First, add a film
-            cy.visit('http://localhost:6969/Staff/Edit/Film');
-            cy.get('#title').type(filmData.title);
-            cy.get('#description').type(filmData.description);
-            cy.get('#category').type(filmData.category);
-            cy.get('#price').type(filmData.price);
-            cy.get('#length').type(filmData.length);
-            cy.get('#rating').type(filmData.rating);
-            cy.get('#release_year').type(filmData.release_year);
-            cy.get('#actors').type(filmData.actors);
-            cy.get('button[type="submit"]').contains('Add Film').click();
-            cy.url({ timeout: 10000 }).should('include', '/Staff/Edit/Film?v=');
             cy.url().then(url => {
-                const filmId = url.split('=')[1];
-                // Now edit the film
-                cy.visit(`http://localhost:6969/Staff/Edit/Film?v=${filmId}`);
-                cy.wait(200); // Wait for input to update
-                cy.get('#title').clear().type('Updated Title');
-                cy.get('button[type="submit"]').contains('Save Changes').click();
-                cy.get('#title').should('have.value', 'Updated Title');
+                filmId = url.split('=')[1];
             });
         });
 
-        it('can delete a film', () => {
-            // First, add a film
-            cy.visit('http://localhost:6969/Staff/Edit/Film');
-            cy.get('#title').type(filmData.title);
-            cy.get('#description').type(filmData.description);
-            cy.get('#category').type(filmData.category);
-            cy.get('#price').type(filmData.price);
-            cy.get('#length').type(filmData.length);
-            cy.get('#rating').type(filmData.rating);
-            cy.get('#release_year').type(filmData.release_year);
-            cy.get('#actors').type(filmData.actors);
-            cy.get('button[type="submit"]').contains('Add Film').click();
-            cy.url({ timeout: 10000 }).should('include', '/Staff/Edit/Film?v=');
-            cy.url().then(url => {
-                const filmId = url.split('=')[1];
-                cy.visit(`http://localhost:6969/Staff/Edit/Film?v=${filmId}`);
-                cy.wait(200); // Wait for input to update
-                cy.get('#delete-film').click();
-                cy.on('window:confirm', () => true);
-                cy.url({ timeout: 10000 }).should('include', '/Catalogue');
-            });
+        it('can edit the created film', () => {
+            cy.visit(`http://localhost:6969/Staff/Edit/Film?v=${filmId}`);
+            cy.wait(200); // Wait for input to update
+            cy.get('#title').clear().type('Updated Title');
+            cy.get('button[type="submit"]').contains('Save Changes').click();
+            cy.wait(200);
+            cy.get('#title').should('have.value', 'Updated Title');
+        });
+
+        it('can delete the created film', () => {
+            cy.visit(`http://localhost:6969/Staff/Edit/Film?v=${filmId}`);
+            cy.wait(200); // Wait for input to update
+            cy.get('#delete-film').click();
+            cy.on('window:confirm', () => true);
+            cy.url({ timeout: 10000 }).should('include', '/Catalogue');
         });
     });
 
